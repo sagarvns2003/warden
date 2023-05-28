7b94fa035d2b56c6298269c36663d485991b9d2a
Vidya Sagar Gupta <v3sagar@gmail.com>
2023-05-28 16:38:33 +0530

    Initial springboot setup

---
+++ src/main/java/io/github/sagarvns2003/warden/WardenApplication.java
@@ -0,0 +1,13 @@
+package io.github.sagarvns2003.warden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardenApplication.class, args);
+	}
+
+}

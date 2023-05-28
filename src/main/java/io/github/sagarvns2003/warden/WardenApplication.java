0a428668bbbf91e71625940af1cb604ed27a9fbe
Vidya Sagar Gupta <v3sagar@gmail.com>
2023-05-28 17:01:20 +0530

    logback corrected.

--- src/main/java/io/github/sagarvns2003/warden/WardenApplication.java
+++ src/main/java/io/github/sagarvns2003/warden/WardenApplication.java
@@ -1,13 +1,31 @@
 package io.github.sagarvns2003.warden;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.config.EnableIntegrationManagement;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@EnableRetry
+@EnableAsync
+@EnableIntegration
+@IntegrationComponentScan
+@EnableIntegrationManagement
 public class WardenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WardenApplication.class, args);
 	}
 
+	@PostConstruct
+	private void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
